6acdc345fc48e13c7b783c0a968b6730299c4d85
jaylenw <mr2cleanj@live.com>
2018-03-17 23:51:59 -0700

    add changes for istanbul and coveralls
    close #2
    close #3
    close #4

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,5 +10,5 @@ export NODE_ENV=test
 echo "NODE_ENV is:"
 echo $NODE_ENV
 
-# runs unit tests
-./node_modules/mocha/bin/mocha --exit
+# runs unit tests and coverage
+./node_modules/istanbul/lib/cli.js cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
