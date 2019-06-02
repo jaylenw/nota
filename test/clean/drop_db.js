421fd9d38ab37f627b78640f88750299fc240f36
jaylenw <mr2cleanj@live.com>
2019-06-02 16:05:17 -0700

    add eslint recommendations to drop_db.js
    close #38

--- test/clean/drop_db.js
+++ test/clean/drop_db.js
@@ -6,15 +6,15 @@ mongoose.Promise = bluebird;
 var databataseURI = config.get('db.databataseURI');
 
 module.exports = function(done) {
-  mongoose.connect(databataseURI, {
-    useMongoClient: true
-  })
-  .then(function () {
-    mongoose.connection.db.dropDatabase();
-    console.log('Dropped test db');
-    done();
-  })
-  .catch(function () {
-    console.log('Failed to drop test db');
-  });
-}
+	mongoose.connect(databataseURI, {
+		useMongoClient: true
+	})
+		.then(function () {
+			mongoose.connection.db.dropDatabase();
+			console.log('Dropped test db');
+			done();
+		})
+		.catch(function () {
+			console.log('Failed to drop test db');
+		});
+};
