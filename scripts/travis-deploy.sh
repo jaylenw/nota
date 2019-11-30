ba4f819b218aa0539acfcce20093e9ab4e8876c6
jaylenw <mr2cleanj@live.com>
2019-11-29 20:18:26 -0800

    provide latest tag when building the deployment image
    close #113

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -5,7 +5,7 @@ environment=$1
 
 if [[ $environment = "dev" || $environment = "prod" ]]; then
   # build an image for deployment from the deployment stage
-  sudo docker build --target deployment -t notaorg/nota-$environment .
+  sudo docker build --target deployment -t notaorg/nota-$environment:latest .
   echo "Docker build completed with exit status of "$?
   # login to docker hub
   echo $DOCKER_ACCESS_TOKEN | sudo docker login --username $DOCKER_USERNAME --password-stdin
