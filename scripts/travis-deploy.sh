29c79c6ef4654215d9782e10942e4d3a2791a0b1
jaylenw <mr2cleanj@live.com>
2019-11-29 20:44:20 -0800

    return the status command of docker push correctly
    related to #113

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -13,8 +13,10 @@ if [[ $environment = "dev" || $environment = "prod" ]]; then
   echo "Docker login completed with exit status of "$?
   # push and tag with first 7 characters of current commit
   sudo docker push notaorg/nota-$environment:$commit_hash
-  echo "Docker push to registry for the $environment environment completed with exit status of "$?
-  exit # leave exit blank to return the exit code of last ran cmd
+  # store the last command's status code
+  push_command_status=$?
+  echo "Docker push to registry for the $environment environment completed with exit status of "$push_command_status
+  exit $push_command_status # return status code of the push command
 else
   echo "Invalid environment string passed. Either 'dev' or 'prod'."
   exit 1; # exit w/ 1 to indicate non successful execution of the script
